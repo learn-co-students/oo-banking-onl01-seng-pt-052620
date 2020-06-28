b6b0b54b08de9e845b4b96a4018ac810d5092f47
Flora Petersen <floramarina@comcast.net>
2020-06-28 03:33:53 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,13 +17,21 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.valid? && @receiver.valid? && @status != "complete"
+    if @sender.valid? && @receiver.valid? && @status != "complete" && @sender.balance > @amount
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
     else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
     end
   end
 end
